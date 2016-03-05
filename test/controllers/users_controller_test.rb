b4856f0f588db88a27a36a508f289a0092a0a7ba
ricesource <hiroki.yonemoto@gmail.com>
2016-03-05 06:23:45 +0000

    Finish 9.49

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,21 +39,9 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
-  test "successful edit with friendly forwarding" do
-    get edit_user_path(@user)
-    log_in_as(@user)
-    assert_redirected_to edit_user_path(@user)
-    name  = "Foo Bar"
-    email = "foo@bar.com"
-    patch user_path(@user), user: { name:  name,
-                                    email: email,
-                                    password:              "",
-                                    password_confirmation: "" }
-    assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal name,  @user.name
-    assert_equal email, @user.email
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
   end
-  
+
 end
